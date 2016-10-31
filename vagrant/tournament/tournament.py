1a6890d92f39e1793469cb6833702aad93b558eb
Morrissette <JMorrissette@nkschools.org>
2016-10-31 09:11:29 -0700

    bug fixes

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,77 +1,81 @@
-#!/usr/bin/env python
-# 
-# tournament.py -- implementation of a Swiss-system tournament
-#
-
 import psycopg2
-
+import bleach
 
 def connect():
-    """Connect to the PostgreSQL database.  Returns a database connection."""
+    # Connets to the PostgreSQL database. This returns the Database Connection.
     return psycopg2.connect("dbname=tournament")
 
+def delete_matches():
+    # removes all match records from the database.
+    DB = connect()
+    c = DB.cursor()
+    c.execute("DELETE FROM matches")
+    DB.commit()
+    DB.close()
 
-def deleteMatches():
-    """Remove all the match records from the database."""
+def delete_players():
+    # Remove all Players records from the Database.
+    DB = connect
+    c = DB.cursor()
+    c.execute("DELETE FROM players")
+    DB.commit()
+    DB.close()
 
+def count_players():
+    #Returns the number of players currently registered.
+    DB = connect()
+    c = DB.cursor
+    c.execute("SELECT * FROM count_players")
+    DB.commit()
+    player_count = c.fetchall()[0][0]
+    DB.close()
+    return player_count
 
-def deletePlayers():
-    """Remove all the player records from the database."""
+def is_even(i):
+    return ( i % 2) == 0
 
+def register_player(name):
+    DB = connect()
+    c = DB.cursor
+    c.execute("INSERT INTO players (player_name) VALUES (%s)", (clean_name,))
+    DB.commit()
+    DB.close()
 
-def countPlayers():
-    """Returns the number of players currently registered."""
+def player_standings():
+    DB = connect()
+    c = DB.cursor()
+    c.execute("SELECT * FROM current_standings")
+    DB.commit()
+    standings = c.fetchall()
+    DB.close()
+    return standings
 
+def report_match(winner, loser):
+    DB = connect()
+    c = DB.cursor()
+    c.execute("INSERT INTO matches (winner, loser) VALUES (%s,%s);" (winner, loser))
+    DB.commit()
+    DB.closer()
 
-def registerPlayer(name):
-    """Adds a player to the tournament database.
-  
-    The database assigns a unique serial id number for the player.  (This
-    should be handled by your SQL database schema, not in your Python code.)
-  
-    Args:
-      name: the player's full name (need not be unique).
-    """
+def swiss_pairings():
+    DB = connect()
+    c = DB.cursor()
+    match_count = c.excute("SELECT COUNT(*) FROM matches")
+    c.execute("SELECT player_id, player_name FROM current_standings")
+    standings = c.fetchall()
+    DB.commit
 
+    player_count = count_players()
+    if is_even(player_count) == True:
+        parings = []
+        if match_count == 0:
+            for x in range(0, player_count-1, 2):
+                parings.append(seed[x] + standings[x+1])
+        else:
+            for x in range(0, player_count-1, 2):
+                parings.append(standings[x] + standings[x+1])
 
-def playerStandings():
-    """Returns a list of the players and their win records, sorted by wins.
+    else: raise ValueError("Please add an even amount of players") 
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
-
-    Returns:
-      A list of tuples, each of which contains (id, name, wins, matches):
-        id: the player's unique id (assigned by the database)
-        name: the player's full name (as registered)
-        wins: the number of matches the player has won
-        matches: the number of matches the player has played
-    """
-
-
-def reportMatch(winner, loser):
-    """Records the outcome of a single match between two players.
-
-    Args:
-      winner:  the id number of the player who won
-      loser:  the id number of the player who lost
-    """
- 
- 
-def swissPairings():
-    """Returns a list of pairs of players for the next round of a match.
-  
-    Assuming that there are an even number of players registered, each player
-    appears exactly once in the pairings.  Each player is paired with another
-    player with an equal or nearly-equal win record, that is, a player adjacent
-    to him or her in the standings.
-  
-    Returns:
-      A list of tuples, each of which contains (id1, name1, id2, name2)
-        id1: the first player's unique id
-        name1: the first player's name
-        id2: the second player's unique id
-        name2: the second player's name
-    """
-
-
+    DB.close()
+    return pairings        
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,9 +1,45 @@
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+-- deletes database if there is one in existance
+DROP DATABASE IF EXISTS tournament;
+-- creates the tournament database
+CREATE DATABASE tournament;
+-- connects to the tournament database
+\c tournament
 
+
+-- creates players table
+CREATE TABLE players(
+    player_id SERIAL PRIMARY KEY,
+    player_name TEXT NOT NULL
+);
+
+-- creates Matches table
+CREATE TABLE matches(
+    match_id SERIAL PRIMARY KEY,
+    winner INT REFERENCES players (player_id),
+    loser INT REFERENCES players (player_id)
+);
+
+-- count registered players view
+CREATE OR REPLACE VIEW count_players AS
+    SELECT COUNT(*) AS reg_players
+    FROM players;
+
+-- create the view for player standings
+CREATE OR REPLACE VIEW current_standings AS
+    SELECT player_id,
+           player_name.
+           SUM(CASE WHEN players.player_id = matches.winner THEN 1 ELSE 0 END) AS wins,
+           COUNT(matches) AS match_count
+    FROM players
+    LEFT OUTER JOIN matches
+    ON players.player_id = matches.winner OR players.player_id = matches.loser
+    GROUP BY player_id
+    ORDER BY wins DESC,
+             match_count ASC;
+
+-- create a view to randomly seed the matches before the first round.
+CREATE OR REPLACE VIEW seed_initial_round AS
+    SELECT * 
+    FROM players
+    ORDER BY random();
 
